34b9400c8938abd966dd70760182a2aa99122d4b
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-01-31 03:32:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -101,33 +101,5 @@ function($scope,ngAnimate,searchFor) {
 }]);
 
 
-app.filter('searchFor', function() {
 
-  // All filters must return a function. The first parameter
-  // is the data that is to be filtered, and the second is an
-  // argument that may be passed with a colon (searchFor:searchString)
-
-  return function(arr, searchString) {
-
-    if (!searchString) {
-      return arr;
-    }
-
-    var result = [];
-
-    searchString = searchString.toLowerCase();
-
-    // Using the forEach helper method to loop through the array
-    angular.forEach(arr, function(item) {
-
-      if (title.header.toLowerCase().indexOf(searchString) !== -1) {
-        result.push(item);
-      }
-
-    });
-
-    return result;
-  };
-
-});
 })(window.angular);
