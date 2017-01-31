9f20786322747644329f5c12ed1957c02972b848
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-01-31 14:57:55 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,35 +4,7 @@
 //var app = angular.module('myApp', ['ngAnimate', 'vAccordion','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages', 'ngRoute', 'ui.router']);
 
 
-.filter('searchFor', function() {
-
-  // All filters must return a function. The first parameter
-  // is the data that is to be filtered, and the second is an
-  // argument that may be passed with a colon (searchFor:searchString)
-
-  return function(arr, searchString) {
-
-    if (!searchString) {
-      return arr;
-    }
-
-    var result = [];
-
-    searchString = searchString.toLowerCase();
-
-    // Using the forEach helper method to loop through the array
-    angular.forEach(arr, function(item) {
-
-      if (title.header.toLowerCase().indexOf(searchString) !== -1) {
-        result.push(item);
-      }
-
-    });
-
-    return result;
-  };
-
-}).controller('MainController',['$scope', 'searchFor',
+.controller('MainController',['$scope', 'searchFor',
 function($scope,searchFor) {
 
 
@@ -97,7 +69,35 @@ function($scope,searchFor) {
     });
   }
 
-}]);
+}]).filter('searchFor', function() {
+
+  // All filters must return a function. The first parameter
+  // is the data that is to be filtered, and the second is an
+  // argument that may be passed with a colon (searchFor:searchString)
+
+  return function(arr, searchString) {
+
+    if (!searchString) {
+      return arr;
+    }
+
+    var result = [];
+
+    searchString = searchString.toLowerCase();
+
+    // Using the forEach helper method to loop through the array
+    angular.forEach(arr, function(item) {
+
+      if (title.header.toLowerCase().indexOf(searchString) !== -1) {
+        result.push(item);
+      }
+
+    });
+
+    return result;
+  };
+
+});
        // .run(['$rootScope',function($rootScope){  console.log("Testing");  }]);
 
 
