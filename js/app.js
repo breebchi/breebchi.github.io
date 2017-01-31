a6dcb551be26b9ef5683855602239cce3b30acb5
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-01-31 15:25:08 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 //angular.module('myApp', ['ngRoute','ngAnimate', 'vAccordion','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'ui.router'])
-  var app =  angular.module('myApp', ['ui.router','ngAnimate','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'searchFor']);
+  var app =  angular.module('myApp', ['ngRoute','ui.router','ngAnimate','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'searchFor']);
 //var app = angular.module('myApp', ['ngAnimate', 'vAccordion','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages', 'ngRoute', 'ui.router']);
 
 
