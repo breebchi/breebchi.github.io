3a5a56c88d919c5f4f366341a14572c5ea8b8598
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-01-31 15:09:38 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function() {
 //angular.module('myApp', ['ngRoute','ngAnimate', 'vAccordion','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'ui.router'])
-  var app =  angular.module('myApp', ['ngAnimate','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'ui.router','searchFor']);
+  var app =  angular.module('myApp', ['ngRoute','ngAnimate','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages',  'ui.router','searchFor']);
 //var app = angular.module('myApp', ['ngAnimate', 'vAccordion','ngAria', 'ui.bootstrap', 'ngMaterial', 'ngMessages', 'ngRoute', 'ui.router']);
 
 
@@ -99,7 +99,7 @@ function($scope) {
   };
 
 });
-       app.run(['$rootScope',function($rootScope){  console.log("Testing");  }]);
+      // app.run(['$rootScope',function($rootScope){  console.log("Testing");  }]);
 
 
 
