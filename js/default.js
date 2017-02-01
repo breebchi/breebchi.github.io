c883a7b7cdfd7a15ca07955ac5c7f946e4a0d14a
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-02-01 13:25:58 +0530

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
         $('ul.nav > li').click(function (e) {
+                console.log(e);
             e.preventDefault();
             $('ul.nav > li').removeClass('active');
             $(this).addClass('active');                
