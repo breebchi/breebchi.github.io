148aefadf734669ab80d6d96ee532c0734c30d51
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-02-01 14:47:14 +0530

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
-//$(document).ready(function () {
-       /* $('ul.nav > li').click(function (e) {
+$(document).ready(function () {
+        $('ul.nav > li').click(function (e) {
                 console.log(e);
             //e.preventDefault();
             $('ul.nav > li').removeClass('active');
@@ -8,26 +8,21 @@
                 return e.target;
         });
         
-        jQuery(window).load(function () {
-             $('ul.nav > li').click(function (e) {
-                 console.log(e.result);
-                    var lastId = "'#"+e.result+"'";
-            $(document.getElementById(lastId)).addClass('active');  
-        });      
-        });
-            /*$('ul.nav > li').click(function (e) {
-                 console.log(e.result);
-                    var lastId = "'#"+e.result+"'";
-            $(document.getElementById(lastId)).addClass('active');  
-        });            */
-        
-        */
+         $('ul.nav > li').click(function (e) {
+                 console.log($(e.result).attr('id'));
+                   // var lastId = "'#"+e.result+"'";
+            //$(document.getElementById(lastId)).addClass('active'); 
+                $(e.result).addClass('active'); 
+               
+        });            
         
         
-        $(".nav a").on("click", function(){
+        
+        
+      /*  $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
    $(this).parent().addClass("active");
-});
-   // });
+});*/
+    });
 
 
