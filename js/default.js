d70c81ca7157b62cc34655d209085c6b0488370f
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-02-01 14:12:40 +0530

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
                 console.log(e);
             e.preventDefault();
             $('ul.nav > li').removeClass('active');
-            $(this).addClass('active');                
+            $(this).addClass('active'); 
+                return this.attr('id');
+        });
+            $('ul.nav > li').click(function (e) {
+                 console.log(e.result);
+                    var lastId = "#"+e.result;
+            $(lastId).addClass('active');  
         });            
     });
