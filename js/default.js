adaa5b5b3ffcd82de2911856368998b17c1d82e2
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-02-10 20:32:42 +0530

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,6 +4,39 @@ $(document).on('click','.navbar-collapse.in',function(e) {
     }
 });
 
+jQuery(document).ready(function() {
+ 
+var offset = 250;
+ 
+var duration = 300;
+ 
+jQuery(window).scroll(function() {
+ 
+if (jQuery(this).scrollTop() > offset) {
+ 
+jQuery(‘.back-to-top’).fadeIn(duration);
+ 
+} else {
+ 
+jQuery(‘.back-to-top’).fadeOut(duration);
+ 
+}
+ 
+});
+ 
+ 
+ 
+jQuery(‘.back-to-top’).click(function(event) {
+ 
+event.preventDefault();
+ 
+jQuery(‘html, body’).animate({scrollTop: 0}, duration);
+ 
+return false;
+ 
+})
+ 
+});
  
  $(document).ready(function(){
 	 
