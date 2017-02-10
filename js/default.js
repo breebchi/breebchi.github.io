77d312cd13ecfc28592de4a822b300691bff26e1
Mahmoud Kraiem <breebchi.com@gmail.com>
2017-02-10 15:06:31 +0530

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,43 +1,30 @@
-$(document).ready(function () {
-       /* $('ul.nav > li').click(function (e) {
-                console.log(e);
-            //e.preventDefault();
-            $('ul.nav > li').find(".active").removeClass('active');
-            $(e.target).addClass('active'); 
-               //return $(this).attr('id');
-                return e.target;
-        });
-        
-         $('ul.nav > li').click(function (e) {
-                 console.log($(e.result).attr('id'));
-                   // var lastId = "'#"+e.result+"'";
-            //$(document.getElementById(lastId)).addClass('active'); 
-                $(e.result).addClass('active'); 
-               
-        });            
-        
-        
-        
-        
-      /*  $(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});*/
-       
-       
-       /*$('.navbar a.navbar-link').click(function() {
-    var navbar_toggle = $('.navbar-toggle');
-    if (navbar_toggle.is(':visible')) {
-        navbar_toggle.trigger('click');
-    }
-});*/
-       
-       
-    
-    });
-
-   $(document).on('click','.navbar-collapse.in',function(e) {
+$(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
     }
 });
+
+ $(document).ready(function(){
+	    // Add smooth scrolling to all links
+	    $("a").on('click', function(event) {
+
+	      // Make sure this.hash has a value before overriding default behavior
+	      if (this.hash !== "") {
+	        // Prevent default anchor click behavior
+	        event.preventDefault();
+
+	        // Store hash
+	        var hash = this.hash;
+
+	        // Using jQuery's animate() method to add smooth page scroll
+	        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	        $('html, body').animate({
+	          scrollTop: $(hash).offset().top
+	        }, 800, function(){
+	     
+	          // Add hash (#) to URL when done scrolling (default click behavior)
+	          window.location.hash = hash;
+	        });
+	      } // End if
+	    });
+	   });
